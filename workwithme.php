2888860444cfa1ee081181b8992e390c39b5a30c
Aashutosh Shrestha <theofficialaman@gmail.com>
2019-03-05 22:04:24 +0545

    After Pulling from another host

---
+++ workwithme.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+<title>Work With Me</title>
+<style type="text/css">
+@import url("design/design.css");
+</style>
+</head>
+<body>
+<div id="header">
+Contact Me:
+<a href="mailto:contact@aashutoshrestha.com.np" target="_blank" style="text-decoration:none">
+<img src="resources/email.png" alt="instagram" width="20" height="20" align="top" onmouseout="this.src='resources/email.png'" onmouseover="this.src='resources/mailhover.png'">contact@aashutoshrestha.com.np</a>
+&amp;
+<a href="tel:+977-986-81-05-448" target="_blank" style="text-decoration:none">
+<img src="resources/telephone.png" alt="instagram" width="20" height="20" align="top" onmouseout="this.src='resources/telephone.png'" onmouseover="this.src='resources/phonehover.png'">+977-986-81-05-448</a>
+
+</div>
+<div id="title">
+<a href="index.php"> <img src="resources/websitemainlogo.png" alt="instagram" width="100" height="60" onmouseout="this.src='resources/websitemainlogo.png'" onmouseover="this.src='resources/logohover.png'">Aashutosh Shrestha
+</a>
+
+</div>
+
+
+<div id="container">
+<div id="right">
+
+<a href="https://www.facebook.com/aashutoshrestha/" target="_blank"> 
+<img src="resources/fb.png" alt="instagram" width="40" height="40" onmouseout="this.src='resources/fb.png'" onmouseover="this.src='resources/fbhover.png'"></a>
+<p>
+<a href="https://www.instagram.com/aashutoshrestha/" target="_blank">
+<img src="resources/insta.png" alt="instagram" width="40" height="40" onmouseout="this.src='resources/insta.png'" onmouseover="this.src='resources/instahover.png'"></a>
+</p>
+<p>
+<a href="https://www.twitter.com/aashutoshrestha/" target="_blank"> 
+<img src="resources/twitter.png" alt="instagram" width="40" height="40" onmouseout="this.src='resources/twitter.png'" onmouseover="this.src='resources/twitterhover.png'"></a>
+</p>
+<p>
+<a href="https://www.github.com/aashutoshrestha/" target="_blank"> 
+<img src="resources/github.png" alt="instagram" width="40" height="40" onmouseout="this.src='resources/github.png'" onmouseover="this.src='resources/githover.png'"></a>
+</p>
+<a href="https://www.linkedin.com/in/aashutosh-shrestha-865010111/" target="_blank"> 
+<img src="resources/linkedin.png" alt="instagram" width="40" height="40" onmouseout="this.src='resources/linkedin.png'" onmouseover="this.src='resources/linkedinhover.png'"></a>
+</div>
+
+
+
+
+  <div id="left">
+  <div id="divmenu">
+<div id="menu">
+<div id="menu" onclick="home()">Home </div>
+</div>
+<div id="menu" onclick="openProjects()">
+Projects
+</div>
+<div id="menu">
+About Me
+</div>
+<div id="menu">
+<div id="currentmenu">
+>>>Work with Me<<<
+</div>
+</div>
+<div id="menu">
+Contact Me
+</div>
+</div>
+  </div>
+  <div align="center">
+
+<p id="text1">
+</p>
+<p id="text2">
+<a href="https://www.amazon.com/God-Not-Great-Religion-Everything/dp/0446697966" target="_blank" style="text-decoration:none">
+</a>
+</p>
+
+<p id="text1">
+</p>
+<p id="text2">
+<a href="workwithme.php" target="_blank" style="text-decoration:none">
+</a>
+</p>
+
+
+
+<p id="text1">
+Drop Your CV
+</p>
+<p id="text2">
+<a href="https://www.dropbox.com/request/5cS6pbNCKhYnywe49OK3" target="_blank" style="text-decoration:none">
+>>For Internship<< </a>
+</p>
+<p id="text2">
+<a href="https://www.dropbox.com/request/5cS6pbNCKhYnywe49OK3" target="_blank" style="text-decoration:none">
+>>For Job<< </a>
+</p>
+<p id="text1">
+No! I have work for you instead.
+</p>
+<p id="text2">
+<a href="mailto:contact@aashutoshrestha.com.np" target="_blank" style="text-decoration:none">
+>>Email Me!<< </a>
+</div>
+</div>
+
+</div>
+
+<div id="footer">
+<p align="center" style="color:white;">&copy; All Rights Reserved, Aashutosh Shrestha 2018  </p>
+</div>
+</body>
+</html>
+<script>
+function home() {
+    window.open("index.php","_self");
+}
+function openProjects() {
+    window.open("projects.php","_self");
+}
+</script>
